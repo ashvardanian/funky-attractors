3b89d0a46cd80542e6887ccd946b4ac69345d4eb
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-02-14 18:03:09 +0400

    Add: Scripts to produce famous shapes

---
+++ examples/generate.sh
@@ -0,0 +1,19 @@
+# From: http://paulbourke.net/fractals/clifford/
+python main.py clifford --a -1.4 --b 1.6 --c 1.0 --d 0.7
+python main.py clifford --a 1.6 --b -0.6 --c -1.2 --d 1.6
+python main.py clifford --a 1.7 --b 1.7 --c 0.6 --d 1.2
+python main.py clifford --a 1.5 --b -1.8 --c 1.6 --d 0.9
+python main.py clifford --a -1.7 --b 1.3 --c -0.1 --d -1.2
+python main.py clifford --a -1.7 --b 1.8 --c -1.9 --d -0.4
+python main.py clifford --a -1.8 --b -2.0 --c -0.5 --d -0.9
+
+# From: http://paulbourke.net/fractals/peterdejong/
+python main.py de_jong --a 1.641 --b 1.902 --c 0.316 --d 1.525
+python main.py de_jong --a 0.970 --b -1.899 --c 1.381 --d -1.506
+python main.py de_jong --a 1.4 --b -2.3 --c 2.4 --d -2.1
+python main.py de_jong --a 2.01 --b -2.53 --c 1.61 --d -0.33
+python main.py de_jong --a -2.7 --b -0.09 --c -0.86 --d -2.2
+python main.py de_jong --a -0.827 --b -1.637 --c 1.659 --d -0.943
+python main.py de_jong --a -2 --b -2 --c -1.2 --d 2
+python main.py de_jong --a -0.709 --b 1.638 --c 0.452 --d 1.740
+python main.py svensson --a 1.4 --b 1.56 --c 1.40 --d -6.56
